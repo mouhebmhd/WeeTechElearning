bad9cb658ab98f2040a86839ed8915c53b873d3c
Mouheb Mehdoui <mouhebhb1920@gmail.com>
2025-03-04 07:50:36 +0100

    launching the server

---
+++ index.js
@@ -0,0 +1,17 @@
+// creating a new express app 
+const express=require("express")
+// configuring the dotenv file 
+const dotenv=require("dotenv")
+dotenv.config()
+//importing applications ports 
+const primaryPort=process.env.primaryPort
+const secondaryPort=process.env.secondaryPort
+const tertiaryPort=process.env.tertiaryPort
+//creating the server 
+const server =express()
+//starting the server
+const port = primaryPort || secondaryPort || tertiaryPort;
+
+server.listen(port, () => {
+    console.log(`Server is actually running on port ${port}`);
+});
